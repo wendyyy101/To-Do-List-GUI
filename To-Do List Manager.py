3b686b3fe71b935c767a1526c679b6273b5dcab6
Wendy Nana Aba Amu Asamoah <wnaamuasamoah@st.knust.edu.gh>
2024-11-29 22:51:40 +0000

    Update To-Do List Manager.py

--- To-Do List Manager.py
+++ To-Do List Manager.py
@@ -65,31 +65,6 @@ root.mainloop()
 
 
 
-#
-# while True:
-#     print("\n--- To-Do List Manager ---")
-#     print("1. Add Task")
-#     print("2. View Tasks")
-#     print("3. Remove Task")
-#     print("4. Exit")
-    
-#     choice = input("Choose an option: ")
-#     if choice == "1":
-#         task = input("Enter the task: ")
-#         add_task(task)
-#     elif choice == "2":
-#         view_tasks()
-#     elif choice == "3":
-#         view_tasks()
-#         try:
-#             task_num = int(input("Enter the task number to remove: "))
-#             remove_task(task_num)
-#         except ValueError:
-#             print("Please enter a valid number!")
-#     elif choice == "4":
-#         print("Goodbye!")
-#         break
-#     else:
-#         print("Invalid choice, please try again.")
+
 
 
